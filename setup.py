129520d1850a592028fed99c0e1e591c18fbec8e
QuilD <quild@mail.ru>
2020-12-30 11:26:00 +0300

    Add long description and packages in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='attrs-serde',
-    version='0.3.1',
+    version='0.3.2',
     description='A serialization addon for attrs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/QuilD/attrs-serde',
     keywords=['attrs', 'serialization', 'dict', 'data', 'nested', 'functional'],
     author='Dotan Nahum',
@@ -12,4 +17,6 @@ setup(
     install_requires=[
         'toolz~=0.11.0', 'cytoolz~=0.11.0', 'attrs~=20.0'
     ],
+    packages=find_packages(),
+    python_requires='>=3.6',
 )
