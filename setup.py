3f88ca80f93a3114765135c79bbf9d066164df78
QuilD <quild@mail.ru>
2021-01-11 11:57:26 +0300

    Update version to 0.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='attrs-serde',
-    version='0.3.2',
+    version='0.3.3',
     description='A serialization addon for attrs',
     long_description=long_description,
     long_description_content_type="text/markdown",
